75520d58f556c2130e6b5aa99fb2ddc2bf7a23af
rine <sirine.elalami.22578@stu.he-ferrer.eu>
2025-05-13 15:36:15 +0200

    no more tailwiwnd, going back to beginn

--- src/js/fullpage.js
+++ src/js/fullpage.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     new fullpage('#fullpage', {
         autoScrolling: true,
-        licenseKey: 'gplv3-license'
-      });
-      
+        licenseKey: 'gplv3-license',
+        menu: '#menu',
+        anchors: ['introduction','presentation', 'varities', 'dark-mode'],
+        scrollOverflow: true,
+        scrolloverflowmacstyle: false,
+    });
 
 });
